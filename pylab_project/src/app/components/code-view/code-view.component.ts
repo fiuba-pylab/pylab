d3a5a78ca0b808d426e4e594054721f4edaf8b42
Agustina Fraccaro <afraccaro@fi.uba.ar>
2024-07-16 22:33:32 -0300

    changes

--- pylab_project/src/app/components/code-view/code-view.component.ts
+++ pylab_project/src/app/components/code-view/code-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, AfterViewInit, Input, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import * as monaco from 'monaco-editor';
 
 @Component({
@@ -7,46 +7,64 @@ import * as monaco from 'monaco-editor';
   styleUrls: ['./code-view.component.css'],
   standalone: true
 })
-export class CodeViewComponent implements AfterViewInit, OnChanges {
-  @Input() code: string = ''; 
-  @Input() language: string = 'python'; 
-  @Input() highlightLine: number | null = null; 
+export class CodeViewComponent implements AfterViewInit, OnChanges, OnDestroy {
+  @Input() code: string = '';
+  @Input() language: string = 'python';
+  @Input() highlightLine: number | null = null;
   private editor: monaco.editor.IStandaloneCodeEditor | null = null;
   private decorationsCollection: monaco.editor.IEditorDecorationsCollection | null = null;
 
   constructor() { }
 
   ngAfterViewInit(): void {
+    this.initEditor();
+  }
+
+  private initEditor(): void {
     this.editor = monaco.editor.create(document.getElementById('editor-container')!, {
       value: this.code,
+      theme: 'vs-dark',
       language: this.language,
       readOnly: true,
       minimap: { enabled: false }
     });
 
     this.decorationsCollection = this.editor.createDecorationsCollection();
+    console.log("decor init: "+this.decorationsCollection);
+    
     this.updateDecorations();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.editor && changes['highlightLine']) {
-      this.updateDecorations();
+      console.log("entro a change");
+      
+      if (this.decorationsCollection) {
+        this.updateDecorations();
+      }
     }
   }
 
   private updateDecorations(): void {
-    console.log("code line: "+ this.highlightLine);
-    
     if (this.editor && this.decorationsCollection) {
+      console.log("entro a update");
+      console.log("code: "+this.highlightLine);
+      
       const newDecorations = this.highlightLine !== null ? [{
-        range: new monaco.Range(this.highlightLine, 1, this.highlightLine, 1),
+        range: new monaco.Range(this.highlightLine, 1, this.highlightLine, this.editor.getModel()?.getLineMaxColumn(this.highlightLine) ?? 1),
         options: {
           isWholeLine: true,
-          className: 'myLineDecoration'
+          inlineClassName: 'myLineDecoration'
         }
       }] : [];
+      console.log(newDecorations);
+      this.decorationsCollection.set(newDecorations)
+    }
+  }
 
-      this.decorationsCollection.set(newDecorations);    
+  ngOnDestroy(): void {
+    if (this.editor) {
+      this.editor.dispose();
     }
   }
 }
--- pylab_project/src/app/pages/program-display/program-display.component.ts
+++ pylab_project/src/app/pages/program-display/program-display.component.ts
@@ -12,82 +12,16 @@ import { CodeViewComponent } from '../../components/code-view/code-view.componen
 })
 export class ProgramDisplayComponent {
   code: string = `function x() {
-    console.log("Hello world!");
-    console.log("Another line");
-    console.log("Yet another line");
-  }`;
-  highlightLine: number | null = 1;
-  index_pointer = 0;
-  lines:string[] = [
-    'def programa(param):',
-    '',
-    '',
-    ''
-    /* 'le1 = len(e1)',
-    'le2 = len(e2)',
-    'if le1>=le2:',
-    'largo = e1',
-    'lmáx = le1',
-    'corto = e2',
-    'lmín = le2',
-    'else:',
-    'largo = e2',
-    'lmáx = le2',
-    'corto = e1',
-    'lmín = le1',
-    'suma = largo.copy()' */
-  ];
-
-  code_descriptions:string[] = [
-    'Definición de la función programa',
-    /* 'le1 = 5',
-    'le2 = 6',
-    'condición le1 mayor o igual le2:',
-    'largo = e1',
-    'lmáx = 5',
-    'corto = e2',
-    'lmín = 6',
-    'else:',
-    'largo = e2',
-    'lmáx = 6',
-    'corto = e1',
-    'lmín = 5',
-    'suma = e2' */
-  ]
-
-  text_descriptions:string[] = [
-    'Definición de la función suma',
-    'Asignación de variable le1',
-    'Asignación de variable le2',
-    'Condición entre variables le1, le2:',
-    'Asignación de variable largo',
-    'lmáx = 5',
-    'Asignación de variable corto',
-    'Asignación de variable lmín',
-    'Si no se cuumple la condición',
-    'Asignación de variable largo',
-    'Asignación de variable lmax',
-    'Asignación de variable corto',
-    'Asignación de variable lmín',
-    'Resultado de suma'
-  ]
-
-  next_index(){
-    if(this.index_pointer < this.lines.length-1){
-      this.index_pointer++;
-    }
-  }
-  previous_index(){
-    if(this.index_pointer > 0){
-      this.index_pointer--;
-    }
-  }
+  console.log("Hello world!");
+  console.log("Another line");
+  console.log("Yet another line");
+}`;
+  highlightLine: number = 1;
 
   nextLine() {
-    console.log("app "+this.highlightLine);
-    
     if (this.highlightLine !== null) {
       this.highlightLine = this.highlightLine < 4 ? this.highlightLine + 1 : 1;
     }
+    console.log("app "+this.highlightLine);
   }
 }
