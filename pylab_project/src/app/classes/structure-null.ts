84639582505d2d6afd6525e3669b3fa724c60162
Agustina Fraccaro <afraccaro@fi.uba.ar>
2024-09-15 00:37:36 -0300

    soporte para funciones anidadas

--- pylab_project/src/app/classes/structure-null.ts
+++ pylab_project/src/app/classes/structure-null.ts
@@ -3,8 +3,8 @@ import { Structure } from "./structure";
 
 const REGEX_VARIABLE_DECLARATION = /(\w+)\s*=\s*(.+)/;
 const REGEX_OPERATIONS = /(\w+)\s*(\+=|-=|\*=|\/=)\s*(.+)/;
-const REGEX_NATIVE_FUNCTIONS = /\b(float|int|len|str)\s*\(([^()]+)\)/g;
-const REGEX_MATH_FUNCTIONS = /math\.(\w+)\(([^()]*(?:\([^()]*\)[^()]*)*)\)/g;
+const REGEX_FUNCTIONS = /\b(float|int|len|str|math\.\w+)\s*\(([^()]+)\)/g;
+const RETURN_BREAK = /\b(return|break)\b(?:\s+[^;]*)?;/g;
 
 
 const operations = {
@@ -30,6 +30,7 @@ export class NullStructure extends Structure {
         }
         const variableDeclaration = this.lines[0].match(REGEX_VARIABLE_DECLARATION);
         const operations = this.lines[0].match(REGEX_OPERATIONS);
+        const print = this.lines[0].match(/print\(([^()]+)\)/);
         if (variableDeclaration) {
             const varName = variableDeclaration[1];
             let varValue = applyFunctions(variableDeclaration[2], this.variables);
@@ -40,6 +41,11 @@ export class NullStructure extends Structure {
             const operator = operations[2];
             const value = operations[3];
             this.variables[variable] = applyOperation(Number(this.variables[variable]), operator, Number(value));
+        }
+
+        if(print){
+            let value = applyFunctions(print[1], this.variables);
+            this.codeService.setPrint(value.replace(/^"(.*)"$/, '$1'));
         }
 
         this.codeService.updateVariables(this.variables);
@@ -58,46 +64,55 @@ function applyOperation(variableValue: number, operator: Operator, value: number
 function applyFunctions(variableValue: any, variables: any) {
     let result = variableValue;
     result = replaceVariables(result, variables);
-    result = applyNativeFunctions(result, variables);
-    result = applyMathFunctions(result, variables);
+    result = evaluateExpression(result);
     return result;
 }
 
-function applyNativeFunctions(variableValue: any, variables: any) {
-    return variableValue.replace(REGEX_NATIVE_FUNCTIONS, (match: any, funcName: any, args: string | any[]) => {
-        switch (funcName) {
-            case 'float':
-                return Number(args);
-            case 'int':
-                return parseInt(args as string); 
-            case 'len':
-                return (args as string).length; 
-            case 'str':
-                return String(args);
-            default:
-                return match; 
-        }
-    });
+function evaluateFunction(funcName: string, args: string): string {
+    // TODO: lógica números imaginarios
+    args = evaluate(args);
+    switch (funcName) {
+        case 'float':
+            return String(Number(args));
+        case 'int':
+            return String(parseInt(args));
+        case 'len':
+            return String((args as string).length); 
+        case 'str':
+            return String(args);
+        case 'math.pow':
+            let funcArgs = (args as string).split(',');
+            return (Math.pow(Number(funcArgs[0]), Number(funcArgs[1]))).toString();
+        case 'math.sqrt':
+            return (Math.sqrt(Number(args))).toString();
+        case 'math.round':
+            funcArgs = (args as string).split(',');
+            if (funcArgs.length > 1) {
+                return Number(funcArgs[0]).toFixed(Number(funcArgs[1])).toString();
+            }
+            return (Math.round(Number(args))).toString();
+        case 'math.asin':
+            return (Math.asin(Number(args))).toString();
+        case 'math.log10':
+            return (Math.log10(Number(args))).toString();
+        default:
+            return args; 
+    }
 }
 
-function applyMathFunctions(variableValue: any, variables: any) {
-    // TODO: lógica números imaginarios
-    return variableValue.replace(REGEX_MATH_FUNCTIONS, (match: any, funcName: any, args: string | any[]) => {
-        switch (funcName) {
-            case 'pow':
-                return Math.pow(Number((args as string).split(',')[0]), Number((args as string).split(',')[1]));
-            case 'sqrt':
-                return Math.sqrt(Number(evaluate(args)));
-            case 'round':
-                return Math.round(Number(evaluate(args)));
-            case 'asin':
-                return Math.asin(Number(evaluate(args)));
-            case 'log10':
-                return Math.log10(Number(evaluate(args)));
-            default:
-                return match; 
-        } 
-    });
+function evaluateExpression(expression: string): string {
+    let previousExpression;
+    let currentExpression = expression;
+
+    do {
+        previousExpression = currentExpression;
+        currentExpression = currentExpression.replace(REGEX_FUNCTIONS, (match, funcName, args) => {
+            let evaluatedArgs = args.split(',').map((arg: string) => evaluateExpression(arg.trim())).join(',');
+            return evaluateFunction(funcName, evaluatedArgs);
+        });
+    } while (currentExpression !== previousExpression);
+
+    return currentExpression;
 }
 
 /* 
