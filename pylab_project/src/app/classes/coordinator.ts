b0d9db7529687fc43204e33d8ef42f15eaa31a30
Agustina Fraccaro <afraccaro@fi.uba.ar>
2024-09-17 19:43:59 -0300

    cambios

--- pylab_project/src/app/classes/coordinator.ts
+++ pylab_project/src/app/classes/coordinator.ts
@@ -10,6 +10,7 @@ export class Coordinator {
     currentLine: number = 0;
     functions: { [key: string]: DefStructure } = {};
     funcCallLine: number = 0;
+    inFunction: boolean = false;
     constructor(codeService: CodeService, code: string) {        
         this.codeService = codeService;
         this.code = code.split('\n');
@@ -24,10 +25,12 @@ export class Coordinator {
     private analize() {
         const matchResult = this.code[this.currentLine].match(/^\s*/);
         const level = matchResult ? matchResult[0].length / 4 : 0;
-        const call = this.code[this.currentLine].match(/([a-zA-Z_][a-zA-Z0-9_]*)\s*\(([^)]*)\)/); // cambiar regex para detectar cuando hay algo asi: a = funcion_propia(param) o q busque las keys de funciones en la linea
-        if(call != null && call[1] in this.functions){
-            this.structures.push(this.functions[call[1]]);
+        //const call = this.code[this.currentLine].match(/([a-zA-Z_][a-zA-Z0-9_]*)\s*\(([^)]*)\)/); // cambiar regex para detectar cuando hay algo asi: a = funcion_propia(param) o q busque las keys de funciones en la linea
+        const call = containsFunctionName(this.code[this.currentLine], this.functions);
+        if(call != null){
+            this.structures.push(this.functions[call]);
             this.funcCallLine = this.currentLine;
+            this.inFunction = true;
             return;
         }
         console.log("VARIABLES: "+JSON.stringify(this.variables));
@@ -43,6 +46,7 @@ export class Coordinator {
         }
         let prevAmount = 0;
         this.analize();
+
         for (let i = this.structures.length - 1; i >= 0; i--){
             const structure = this.structures[i];
             const result = structure.execute(prevAmount);
@@ -54,4 +58,13 @@ export class Coordinator {
             this.codeService.nextLine(result.amount);
         }
     }
+}
+
+function containsFunctionName(str: string, dict: {[key: string]: any}): string | null{
+    for (let key in dict) {
+        if (str.includes(key)) {
+            return key; 
+        }
+    }
+    return null;
 }
