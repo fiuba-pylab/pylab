976852348698b977d523ab9d87bce652660e8db0
Agustina Fraccaro <afraccaro@fi.uba.ar>
2024-10-17 20:16:21 -0300

    refactor variable view to show collections correctly

--- pylab_project/src/app/components/variable-view/variable-view.component.ts
+++ pylab_project/src/app/components/variable-view/variable-view.component.ts
@@ -24,6 +24,8 @@ export class VariableViewComponent {
       console.log("Contextos: "+value);
       this.contexts = value;
       this.contextsKeys = Array.from(this.contexts.keys());
+      console.log(this.contexts);
+      
     });
 
     this.codeService.print.subscribe((value) => {
@@ -35,6 +37,21 @@ export class VariableViewComponent {
     const dictionary = this.contexts.get(context);
     return dictionary ? Object.keys(dictionary) : [];
   }
+
+  getVariableValues(context: Context, key: string): any {
+    const dictionary = this.contexts.get(context);
+    console.log(dictionary);
+  
+    if (dictionary) {
+      const value = dictionary[key];
+      if(value?.print){
+        return value.print();
+      }
+      return value;
+    }
+    return '';
+  }
+  
 
   // ngAfterViewInit(): void {
   //   this.jsPlumbInstance = jsPlumb.getInstance();
