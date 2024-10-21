44975db94b363a0f3fb643f54f16e57711c44c75
Agustina <aschmidt@fi.uba.ar>
2024-10-20 22:36:51 -0300

    rediseño estilos output y variables

--- pylab_project/src/app/pages/program-display/program-display.component.ts
+++ pylab_project/src/app/pages/program-display/program-display.component.ts
@@ -48,6 +48,10 @@ export class ProgramDisplayComponent implements OnInit {
         });
       }
     );
+  }
+
+  ngAfterViewInit() {
+    window.scrollTo(0, 0);
     this.openDialog(this.program);
   }
 
