f6f1adb9f03289fa054e5beeb2703aa3cefcf487
Pedro Aguilar <paguilar@fi.uba.ar>
2024-10-21 21:04:34 -0300

    fix dictionary bug

--- pylab_project/src/app/classes/collection.ts
+++ pylab_project/src/app/classes/collection.ts
@@ -1,7 +1,7 @@
 export abstract class Collection{
     values:any[] | any
     constructor(initialValues?:any[] | any){
-        this.values = (initialValues[0]  === '' ? [] : initialValues)
+        this.values = (initialValues && initialValues[0]  === '' ? [] : initialValues)
     }
     
     abstract add(element:any):void
