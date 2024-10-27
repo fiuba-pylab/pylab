49c7f22f8161e853425581d039a561ac51f28b88
Pedro Aguilar <paguilar@fi.uba.ar>
2024-10-27 17:34:27 -0300

    fix collection element divition

--- pylab_project/src/app/classes/structure-null.ts
+++ pylab_project/src/app/classes/structure-null.ts
@@ -211,7 +211,7 @@ export class NullStructure extends Structure {
             }
             return new List(values);
         } else if (varMatch = varValue.match(REGEX_CONSTS.REGEX_DICTIONARY)) {
-            const dictionaryElements = varMatch[1].replace(', ', ',').split(',');
+            const dictionaryElements = varMatch[1].replace(/\, /g, ',').split(',');
             const dictionary = new Dictionary();
             let element;
             for (element of dictionaryElements) {
@@ -219,10 +219,10 @@ export class NullStructure extends Structure {
             }
             return dictionary;
         } else if (varValue.match(REGEX_CONSTS.REGGEX_SET)) {
-            const values = varValue.slice(1, varValue.length - 1).replace(', ', ',').split(',');
+            const values = varValue.slice(1, varValue.length - 1).replace(/\, /g, ',').split(',');
             return new Set(values);
         } else if (varValue.match(REGEX_CONSTS.REGGEX_TUPLE)) {
-            const values = varValue.slice(1, varValue.length - 1).replace(', ', ',').split(',');
+            const values = varValue.slice(1, varValue.length - 1).replace(/\, /g, ',').split(',');
             return new Tuple(values);
         }  else {
             return null;
