18fbb47dfa474453d18d010fe8cb8edf30e45ec9
Pedro Andrés Flynn <pedro.flynn@middlesoft.com.uy>
2024-10-30 17:59:25 -0300

    cambiar docu a español

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -52,7 +52,7 @@ const ABS = 'abs';
 const validAddOperators = ['append', 'add'];
 const validSubstractOperators = ['remove', 'discard'];
 
-export const PYTHON_DOC_URL = "https://docs.python.org/3/library/functions.html#"
+export const PYTHON_DOC_URL = "https://docs.python.org/es/3.13/library/functions.html#"
 
 export const REGEX_CONSTS = {
     REGEX_DEF, 
