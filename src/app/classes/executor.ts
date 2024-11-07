37dada7946900068578f9eda4fdc00676b1c5c58
Pedro Aguilar <paguilar@fi.uba.ar>
2024-11-06 22:21:42 -0300

    more modular refactor

--- src/app/classes/executor.ts
+++ src/app/classes/executor.ts
@@ -8,14 +8,8 @@ import { Dictionary } from "./dictionary";
 import { List } from "./list";
 import {Set} from "./set"
 import { Tuple } from "./tuple";
+import {replaceVariablesInPrint, cleanPrintValue, matchCollection, applyOperation, evaluateExpression, applyFunctions} from "./executor-utils"
 
-const operations = {
-    '+=': (a: number, b: number) => a + b,
-    '-=': (a: number, b: number) => a - b,
-    '*=': (a: number, b: number) => a * b,
-    '//=': (a: number, b: number) => Math.floor(a / b),
-    '/=': (a: number, b: number) => a / b,
-};
 const collectionOperations: { [key: string]: (set: Set, ...sets: Set[]) => Set } = {
     intersection: (set, ...sets) => set.intersection(...sets),
     difference: (set, ...sets) => set.difference(...sets),
@@ -30,8 +24,12 @@ const collectionSetOps = {
     '|': (set: Set, values: []) => values.forEach((value: string) => set.add(value)),
 };
 
-type Operator = keyof typeof operations;
 type SetOperator = keyof typeof collectionSetOps;
+
+
+
+
+
 export class Executor{
     lines:string[];
     codeService: CodeService | null;
@@ -47,176 +45,25 @@ export class Executor{
         this.variables = this.variablesService!.getVariables(this.context);
     }
 
-    async applyFunctions(variableValue: any, variables: any, varName?: string): Promise<any> {
-        let result = variableValue;
-        result = replaceVariables(result, variables);
-        result = await this.evaluateExpression(result, varName);
-        result = result.replace(/False/g, 'false').replace(/True/g, 'true')
-        return result;
-    }
-
-    async evaluateFunction(funcName: string, args: string, varName?: string): Promise<string> {
-        let evalArgs = evaluate(args);
-        switch (funcName) {
-            case NATIVE_FUNCTIONS.FLOAT:
-                return String(Number(evalArgs));
-            case NATIVE_FUNCTIONS.INT:
-                return String(parseInt(evalArgs));
-            case NATIVE_FUNCTIONS.STR:
-                return String(evalArgs);
-            case NATIVE_FUNCTIONS.MATH_POW:
-                var funcArgs = (args as string).split(',');
-                return (Math.pow(Number(funcArgs[0]), Number(funcArgs[1]))).toString();
-            case NATIVE_FUNCTIONS.MATH_SQRT:
-                let imaginary = false
-                if (evalArgs < 0) {
-                    evalArgs = evalArgs * -1;
-                    imaginary = true
-                }
-                return ((Math.sqrt(Number(evalArgs))).toString() + (imaginary ? 'i' : ''));
-            case NATIVE_FUNCTIONS.MATH_ROUND:
-                var funcArgs = (args as string).split(',');
-                if (funcArgs.length > 1) {
-                    return Number(funcArgs[0]).toFixed(Number(funcArgs[1])).toString();
-                }
-                return (Math.round(Number(evalArgs))).toString();
-            case NATIVE_FUNCTIONS.MATH_ASIN:
-                return (Math.asin(Number(evalArgs))).toString();
-            case NATIVE_FUNCTIONS.MATH_LOG10:
-                return (Math.log10(Number(evalArgs))).toString();
-            case NATIVE_FUNCTIONS.LEN:
-                return String((evalArgs as string).length);
-            case NATIVE_FUNCTIONS.INPUT:
-                return await this.codeService!.getInput(evalArgs, varName ?? '');
-            case NATIVE_FUNCTIONS.ABS:
-                return (Math.abs(Number(evalArgs))).toString();
-            default:
-                return evalArgs;
+    async manageInput(evalArgs:string){
+        let input_params = evalArgs?.split('~~')
+        if(input_params[0]  == '/INPUT/'){
+            return await this.codeService!.getInput(input_params[1], input_params[2] ?? '');
         }
-    }
-
-    async matchCollection(varValue: string, variables: any, collectionName: string) {
-        let collectionAccess;
-        if (collectionAccess = collectionName.match(REGEX_CONSTS.REGEX_COLLECTION_ACCESS)) {
-            const value = collectionAccess[4]? collectionAccess[4] : collectionAccess[2];
-            const index = collectionAccess[5]? collectionAccess[5] : collectionAccess[3];
-            const accessIndex = evaluate(await this.applyFunctions(index, variables));
-            //caso en que se indexa un string
-            if(typeof(variables[value]) == 'string'){
-                return variables[value][Number(index)]
-            }
-            var valueFromCollection = variables[value].access(accessIndex) ? variables[value].access(accessIndex) : 'None' 
-            if(collectionAccess[1]){
-                return Number(valueFromCollection);
-            }
-            return valueFromCollection;
-        }
-        const variable = variables[varValue]
-        if(variable && variable instanceof Collection){
-            varValue = variable.values
-        }
-        let varMatch;
-        if (varValue.match(REGEX_CONSTS.REGEX_LIST)) {
-            const values:any = varValue.slice(1, varValue.length - 1).replace(/\, /g, ',').split(',');
-            for(let i = 0; i<values.length; i++){
-                let variable;
-                if(variables && (variable = variables[values[i]])){
-                    values[i] = variable
-                }
-                
-            }
-            return new List(values);
-        } else if (varMatch = varValue.match(REGEX_CONSTS.REGEX_DICTIONARY)) {
-            const dictionaryElements = varValue.slice(1, varValue.length - 1).replace(/\, /g, ',').split(',');
-            const dictionary = new Dictionary();
-            let element;
-            if(dictionaryElements[0] != ''){
-                for (element of dictionaryElements) {
-                    dictionary.add(element.toString());
-                }
-            }
-            return dictionary;
-        } else if (varValue.match(REGEX_CONSTS.REGGEX_SET)) {
-            if(varValue == 'set()')
-                return new Set();
-            const values = varValue.slice(1, varValue.length - 1).replace(/\, /g, ',').split(',');
-            return new Set(values);
-        } else if (varValue.match(REGEX_CONSTS.REGGEX_TUPLE)) {
-            const values = varValue.slice(1, varValue.length - 1).replace(/\, /g, ',').split(',');
-            return new Tuple(values);
-        }  else {
-            return null;
-        }
-    }
-
-    applyOperation(variableValue: number, operator: Operator, value: number): number {
-        if (operator in operations) {
-            return operations[operator](variableValue, value);
-        } else {
-            throw new Error('Operador no soportado');
-        }
-    }
-
-    async evaluateExpression(expression: string, varName?: string): Promise<string> {
-        let previousExpression;
-        let currentExpression = expression;
-
-        do {
-            previousExpression = currentExpression;
-            currentExpression = await this.replaceAsync(currentExpression, REGEX_CONSTS.REGEX_FUNCTIONS, async (match, funcName, args) => {
-                let evaluatedArgs = await Promise.all(args.split(',').map(async (arg: string) => await this.evaluateExpression(arg.trim(), varName)));
-                return await this.evaluateFunction(funcName, evaluatedArgs.join(','), varName);
-            });
-        } while (currentExpression !== previousExpression);
-
-        return currentExpression;
-    }
-
-    async replaceAsync(str: string, regex: RegExp, asyncFn: (match: string, ...args: any[]) => Promise<string>): Promise<string> {
-        const promises: Promise<string>[] = [];
-        str.replace(regex, (match, ...args) => {
-            const promise = asyncFn(match, ...args);
-            promises.push(promise);
-            return match;
-        });
-        const data = await Promise.all(promises);
-        return str.replace(regex, () => data.shift()!);
-    }
-
-
-    cleanPrintValue(value: string): string {
-        value = value.replace(/^[^'"]*['"]/, '');
-        value = value.replace(/^"|'(.*)"|'$/, '$1');
-        value = value.replace(/["']/g, '');
-        value = value.replace(/\\n|\n/g, '<br>');
-        value = value.replace(/\\t|\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
-        return value;
-    }
-
-    replaceVariablesInPrint(template: string, valores: { [clave: string]: any }): string {
-        return Object.entries(valores).reduce((resultado, [clave, valor]) => {
-            if(valor instanceof Collection){
-                valor = valor.values
-            }
-            const regex = new RegExp(`\\{\\b${this.printVarRegex(clave)}\\b\\}`, 'g');
-            return resultado.replace(regex, valor);
-        }, template);
-    }
-
-    printVarRegex(string: string): string {
-        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    }
+        return evalArgs
+    } 
 
     async checkPrint(){
+        
         const print = this.lines[0].match(REGEX_CONSTS.REGEX_PRINT);
         if (print) {
             let value = print[1]
             if (print[2]) {
                 value = print[2]
             }
-            let printValue = this.replaceVariablesInPrint(value, this.variables);
-            printValue = await this.evaluateExpression(printValue);
-            printValue = this.cleanPrintValue(printValue);
+            let printValue = replaceVariablesInPrint(value, this.variables);
+            printValue = await evaluateExpression(printValue);
+            printValue = cleanPrintValue(printValue);
 
             const end = printValue.match(REGEX_CONSTS.REGEX_PRINT_END);
             if(end){
@@ -254,7 +101,7 @@ export class Executor{
             }
  
              if(collectionsIn){
-                 const elemento = await this.applyFunctions(collectionsIn[1], this.variables);
+                 const elemento = await this.manageInput(await applyFunctions(collectionsIn[1], this.variables));
                  const variable = collectionsIn[2];
                  if(this.variables[variable].in(elemento.replace(/^'|'$/g, ''))){
                     this.variables[varName] = 'True';
@@ -276,8 +123,8 @@ export class Executor{
                      return { amount: 1, finish: true };
                  }  
              }
-             varValue = await this.applyFunctions(variableDeclaration[2], this.variables, varName);
-             let collection = await this.matchCollection(varValue, this.variables, variableDeclaration[2]);
+             varValue = await this.manageInput(await applyFunctions(variableDeclaration[2], this.variables, varName));
+             let collection = await matchCollection(varValue, this.variables, variableDeclaration[2]);
           
              if (!collection) {
                  this.variables[varName] = evaluate(varValue);
@@ -311,8 +158,8 @@ export class Executor{
             const variable = operations[1];
             const operator:any = operations[2];
             let value = operations[3];
-            value = await this.applyFunctions(value, this.variables);
-            this.variables[variable] = this.applyOperation(Number(replaceVariables(variable, this.variables)), operator, Number(evaluate(value)));
+            value = await this.manageInput(await applyFunctions(value, this.variables));
+            this.variables[variable] = applyOperation(Number(replaceVariables(variable, this.variables)), operator, Number(evaluate(value)));
         }
         this.variablesService!.setVariables(this.context, this.variables);
         return null
@@ -325,7 +172,7 @@ export class Executor{
             const operator = collectionAdd[2];
             const value = collectionAdd[3];
             if (VALID_OPERATORS.validAddOperators.includes(operator)) {
-                this.variables[variable].add(await this.applyFunctions(value, this.variables, variable))
+                this.variables[variable].add(await this.manageInput(await applyFunctions(value, this.variables, variable)))
             } else if (collectionAdd[5] == '+') {
                 let tuple;
                 let values = []
@@ -350,7 +197,7 @@ export class Executor{
             const operator = collectionSubstract[2];
             const value = collectionSubstract[3];
             if (VALID_OPERATORS.validSubstractOperators.includes(operator)) {
-                this.variables[variable].substract(await this.applyFunctions(value, this.variables, variable))
+                this.variables[variable].substract(await this.manageInput(await applyFunctions(value, this.variables, variable)))
             }
         }
         this.variablesService!.setVariables(this.context, this.variables);
@@ -363,7 +210,7 @@ export class Executor{
             let values = isReturn[1].split(',').map((value: string) => value.trim());
             if (values) {
                 for (let i = 0; i < values.length; i++) {
-                    let value = await this.applyFunctions(values[i], this.variables)
+                    let value = await this.manageInput(await applyFunctions(values[i], this.variables))
                     values[i] = evaluate(value);
                 }
                 this.context.setReturnValue(values);
@@ -381,9 +228,9 @@ export class Executor{
             let collection:Collection
             if(collection = this.variables[varnName]){
                 const values = this.lines[0].split('=')[1]
-                let varValue = evaluate(await this.applyFunctions(values, this.variables));
+                let varValue = evaluate(await this.manageInput(await applyFunctions(values, this.variables)));
                 const index_values = collection_index.split('[')[1].slice(0, -2)
-                const evaluate_index = evaluate(await this.applyFunctions(index_values, this.variables))
+                const evaluate_index = evaluate(await this.manageInput(await applyFunctions(index_values, this.variables)))
                 collection.insert(evaluate_index, varValue)
             }
         }
