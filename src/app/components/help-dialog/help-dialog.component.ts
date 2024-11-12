df51b8f8db27e4cc189326a215121b66d4b0a2b0
Agustina <aschmidt@fi.uba.ar>
2024-11-12 16:40:53 -0300

    modal helpDialog: fix titulo y redirección a página principal

--- src/app/components/help-dialog/help-dialog.component.ts
+++ src/app/components/help-dialog/help-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-help-dialog',
@@ -14,11 +15,15 @@ export class HelpDialogComponent {
 
   constructor(
     public dialogRef: MatDialogRef<HelpDialogComponent>,
+    private router: Router,
   ) { }
 
   ngOnInit() {
   }
 
+  navigateToHome() {
+    this.router.navigate(['/pylab-deploy/home']);
+  }
 
 
 }
